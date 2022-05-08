469583cf4c005bff781ce32e703b2308e95ccca1
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-05-08 19:09:10 +0530

    Create matrix_search.cpp

---
+++ matrix_search.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+const int m = 10;
+const int n = 10;
+class Matrix
+{
+    public:
+        int dove_tail_search(int M[m][n],int key,int R,int C)
+        {
+            
+        }
+};
